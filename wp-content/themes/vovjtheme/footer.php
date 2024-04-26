0827fd1b27529f6980f2602c2cd50dbb662d926e
abhijith <abhijith@pixelflames.com>
2024-04-26 12:59:12 +0530

    new updates

--- wp-content/themes/vovjtheme/footer.php
+++ wp-content/themes/vovjtheme/footer.php
@@ -7,18 +7,26 @@
                <div class="row align-items-center justify-content-between">
                    <div class="col-lg-6">
                        <h2 class="footer-top-title">
-                           Let’s Work Together
+                           <?php echo get_field("footer_heading", "option"); ?>
                        </h2>
                    </div>
                    <div class="col-lg-5">
                        <div class="footer-top-wrap">
-                           <p class="mb-30">We are digital agency that helps businesses develop immersive and engaging user experiences</p>
-                           <a href="contact.html" class="btn">
-                               <span class="link-effect">
-                                   <span class="effect-1">LET'S TALK WITH US</span>
-                                   <span class="effect-1">LET'S TALK WITH US</span>
-                               </span>
-                           </a>
+                           <p class="mb-30"><?php echo get_field("footer_description", "option"); ?></p>
+                           <?php
+                            $footer_cta = get_field('footer_cta', 'option');
+                            if ($footer_cta) :
+                                $link_url = $footer_cta['url'];
+                                $link_title = $footer_cta['title'];
+                                $link_target = $footer_cta['target'] ? $footer_cta['target'] : '_self';
+                            ?>
+                               <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>" class="btn">
+                                   <span class="link-effect">
+                                       <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                       <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                   </span>
+                               </a>
+                           <?php endif; ?>
                        </div>
                    </div>
                </div>
@@ -26,60 +34,50 @@
            <div class="footer-menu-area">
                <div class="row gy-3 justify-content-between">
                    <div class="col-xxl-6 col-lg-7">
-                       <ul class="footer-menu-list">
-                           <li>
-                               <a href="about.html">
-                                   <span class="link-effect">
-                                       <span class="effect-1">ABOUT COMPANY</span>
-                                       <span class="effect-1">ABOUT COMPANY</span>
-                                   </span>
-                               </a>
-                           </li>
-                           <li>
-                               <a href="project.html">
-                                   <span class="link-effect">
-                                       <span class="effect-1">OUR CAREERS</span>
-                                       <span class="effect-1">OUR CAREERS</span>
-                                   </span>
-                               </a>
-                           </li>
-                           <li>
-                               <a href="service.html">
-                                   <span class="link-effect">
-                                       <span class="effect-1">SERVICES</span>
-                                       <span class="effect-1">SERVICES</span>
-                                   </span>
-                               </a>
-                           </li>
-                           <li>
-                               <a href="contact.html">
-                                   <span class="link-effect">
-                                       <span class="effect-1">CONTACT</span>
-                                       <span class="effect-1">CONTACT</span>
-                                   </span>
-                               </a>
-                           </li>
-                       </ul>
+                       <?php if (have_rows('footer_quick_links', 'option')) : ?>
+                           <ul class="footer-menu-list">
+                               <?php while (have_rows('footer_quick_links', 'option')) : the_row();
+                                    $link = get_sub_field('link');
+                                    if ($link) :
+                                        $link_url = $link['url'];
+                                        $link_title = $link['title'];
+                                        $link_target = $link['target'] ? $link['target'] : '_self';
+                                    endif;
+                                ?>
+                                   <li>
+                                       <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                                           <span class="link-effect">
+                                               <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                               <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                           </span>
+                                       </a>
+                                   </li>
+                               <?php endwhile; ?>
+                           </ul>
+                       <?php endif; ?>
                    </div>
                    <div class="col-xxl-6 col-lg-5 text-lg-end">
-                       <ul class="footer-menu-list">
-                           <li>
-                               <a href="about.html">
-                                   <span class="link-effect">
-                                       <span class="effect-1">PRIVACY POLICY</span>
-                                       <span class="effect-1">PRIVACY POLICY</span>
-                                   </span>
-                               </a>
-                           </li>
-                           <li>
-                               <a href="project.html">
-                                   <span class="link-effect">
-                                       <span class="effect-1">TERMS & CONDITIONS</span>
-                                       <span class="effect-1">TERMS & CONDITIONS</span>
-                                   </span>
-                               </a>
-                           </li>
-                       </ul>
+                       <?php if (have_rows('footer_other_links', 'option')) : ?>
+                           <ul class="footer-menu-list">
+                               <?php while (have_rows('footer_other_links', 'option')) : the_row();
+                                    $link = get_sub_field('link');
+                                    if ($link) :
+                                        $link_url = $link['url'];
+                                        $link_title = $link['title'];
+                                        $link_target = $link['target'] ? $link['target'] : '_self';
+                                    endif;
+                                ?>
+                                   <li>
+                                       <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                                           <span class="link-effect">
+                                               <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                               <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                           </span>
+                                       </a>
+                                   </li>
+                               <?php endwhile; ?>
+                           </ul>
+                       <?php endif; ?>
                    </div>
                </div>
            </div>
@@ -89,35 +87,25 @@
                <div class="row gy-3 justify-content-between align-items-center">
                    <div class="col-md-6">
                        <div class="social-btn style3">
-                           <a href="https://www.facebook.com/">
-                               <span class="link-effect">
-                                   <span class="effect-1"><i class="fab fa-facebook"></i></span>
-                                   <span class="effect-1"><i class="fab fa-facebook"></i></span>
-                               </span>
-                           </a>
-                           <a href="https://instagram.com/">
-                               <span class="link-effect">
-                                   <span class="effect-1"><i class="fab fa-instagram"></i></span>
-                                   <span class="effect-1"><i class="fab fa-instagram"></i></span>
-                               </span>
-                           </a>
-                           <a href="https://twitter.com/">
-                               <span class="link-effect">
-                                   <span class="effect-1"><i class="fab fa-twitter"></i></span>
-                                   <span class="effect-1"><i class="fab fa-twitter"></i></span>
-                               </span>
-                           </a>
-                           <a href="https://dribbble.com/">
-                               <span class="link-effect">
-                                   <span class="effect-1"><i class="fab fa-dribbble"></i></span>
-                                   <span class="effect-1"><i class="fab fa-dribbble"></i></span>
-                               </span>
-                           </a>
+                           <?php if (have_rows('header_social_media', 'option')) : ?>
+                               <?php while (have_rows('header_social_media', 'option')) : the_row();
+                                    $type = get_sub_field('type');
+                                    $link = get_sub_field('link');
+
+                                ?>
+                                   <a href="<?php echo esc_url($link); ?>" target="_blank">
+                                       <span class="link-effect">
+                                           <span class="effect-1"><i class="fab fa-<?php echo $type; ?>"></i></span>
+                                           <span class="effect-1"><i class="fab fa-<?php echo $type; ?>"></i></span>
+                                       </span>
+                                   </a>
+                               <?php endwhile; ?>
+                           <?php endif; ?>
+
                        </div>
                    </div>
                    <div class="col-md-6 align-self-center text-lg-end">
-                       <p class="copyright-text">Copyright © 2024
-                           <a href="https://themeforest.net/user/themegenix">ThemeGenix</a>
+                       <p class="copyright-text"><?php echo get_field('copy_right_text', 'option'); ?>
                        </p>
                    </div>
                </div>
--- wp-content/themes/vovjtheme/header.php
+++ wp-content/themes/vovjtheme/header.php
@@ -33,64 +33,64 @@
         <div class="mobile-menu-area">
             <button class="menu-toggle"><i class="fas fa-times"></i></button>
             <div class="mobile-logo">
-                <a href="index.html"><img src="assets/img/logo.svg" alt="Ovation"></a>
+                <a href="/"><img src="<?php $header_logo = get_field("header_logo", 'option');
+                                        echo esc_url($header_logo['url']); ?>" alt="<?php echo esc_attr($header_logo['alt']); ?>"></a>
             </div>
             <div class="mobile-menu">
-                <ul>
-                    <li class="">
-                        <a href="#">Home</a>
+                <?php if (have_rows('header_navigation', 'option')) : ?>
+                    <ul>
+                        <?php while (have_rows('header_navigation', 'option')) : the_row();
+                            $link = get_sub_field('link');
+                            if ($link) :
+                                $link_url = $link['url'];
+                                $link_title = $link['title'];
+                                $link_target = $link['target'] ? $link['target'] : '_self';
+                            endif;
+                        ?>
+                            <li class="">
+                                <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                                    <?php echo esc_html($link_title); ?>
+                                </a>
 
-                    </li>
-                    <li class="">
-                        <a href="#">Pages</a>
+                            </li>
+                        <?php endwhile; ?>
+                    </ul>
+                <?php endif; ?>
 
-                    </li>
-                    <li class="">
-                        <a href="#">Portfolio</a>
-
-                    </li>
-                    <li class="">
-                        <a href="#">Blog</a>
-
-                    </li>
-                    <li>
-                        <a href="contact.html">Contact</a>
-                    </li>
-                </ul>
             </div>
+            <?php
+            $contact_address = get_field('contact_address', 'option');
+            $contact_number = get_field('contact_number', 'option');
+            $contact_email = get_field('contact_email', 'option');
+
+            ?>
+            <?php if ($contact_address) : ?>
+                <div class="sidebar-wrap">
+                    <?php echo $contact_address; ?>
+                </div>
+            <?php endif; ?>
             <div class="sidebar-wrap">
-                <h6>27 Division St, New York,</h6>
-                <h6>NY 10002, USA</h6>
-            </div>
-            <div class="sidebar-wrap">
-                <h6><a href="tel:1800123654987">+1 800 123 654 987 </a></h6>
-                <h6><a href="mailto:frisk.agency@mail.com">frisk.agency@mail.com</a></h6>
+                <h6><a href="tel:<?php echo $contact_number; ?>"><?php echo $contact_number; ?> </a></h6>
+                <h6><a href="mailto:<?php echo $contact_email; ?>"><?php echo $contact_email; ?></a></h6>
             </div>
             <div class="social-btn style3">
-                <a href="https://www.facebook.com/">
-                    <span class="link-effect">
-                        <span class="effect-1"><i class="fab fa-facebook"></i></span>
-                        <span class="effect-1"><i class="fab fa-facebook"></i></span>
-                    </span>
-                </a>
-                <a href="https://instagram.com/">
-                    <span class="link-effect">
-                        <span class="effect-1"><i class="fab fa-instagram"></i></span>
-                        <span class="effect-1"><i class="fab fa-instagram"></i></span>
-                    </span>
-                </a>
-                <a href="https://twitter.com/">
-                    <span class="link-effect">
-                        <span class="effect-1"><i class="fab fa-twitter"></i></span>
-                        <span class="effect-1"><i class="fab fa-twitter"></i></span>
-                    </span>
-                </a>
-                <a href="https://dribbble.com/">
-                    <span class="link-effect">
-                        <span class="effect-1"><i class="fab fa-dribbble"></i></span>
-                        <span class="effect-1"><i class="fab fa-dribbble"></i></span>
-                    </span>
-                </a>
+
+                <?php if (have_rows('header_social_media', 'option')) : ?>
+                    <?php while (have_rows('header_social_media', 'option')) : the_row();
+                        $type = get_sub_field('type');
+                        $link = get_sub_field('link');
+                      
+                    ?>
+                        <a href="<?php echo esc_url($link); ?>" target="_blank">
+                            <span class="link-effect">
+                                <span class="effect-1"><i class="fab fa-<?php echo $type; ?>"></i></span>
+                                <span class="effect-1"><i class="fab fa-<?php echo $type; ?>"></i></span>
+                            </span>
+                        </a>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+
+
             </div>
         </div>
     </div>
@@ -105,57 +105,35 @@
                     <div class="row align-items-center justify-content-between">
                         <div class="col-auto">
                             <div class="header-logo">
-                                <a href="/"><img src="assets/img/logo.svg" alt="logo"></a>
+                                <a href="/"><img src="<?php $header_logo = get_field("header_logo", 'option');
+                                                        echo esc_url($header_logo['url']); ?>" alt="<?php echo esc_attr($header_logo['alt']); ?>"></a>
                             </div>
                         </div>
                         <div class="col-auto ms-auto">
                             <nav class="main-menu d-none d-lg-inline-block">
-                                <ul>
-                                    <li class="active ">
-                                        <a href="#">
-                                            <span class="link-effect">
-                                                <span class="effect-1">HOME</span>
-                                                <span class="effect-1">HOME</span>
-                                            </span>
-                                        </a>
+                                <?php if (have_rows('header_navigation', 'option')) : ?>
+                                    <ul>
+                                        <?php while (have_rows('header_navigation', 'option')) : the_row();
+                                            $link = get_sub_field('link');
+                                            if ($link) :
+                                                $link_url = $link['url'];
+                                                $link_title = $link['title'];
+                                                $link_target = $link['target'] ? $link['target'] : '_self';
+                                            endif;
+                                        ?>
+                                            <li class="">
+                                                <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                                                    <span class="link-effect">
+                                                        <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                                        <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                                    </span>
+                                                </a>
 
-                                    </li>
-                                    <li class="">
-                                        <a href="#">
-                                            <span class="link-effect">
-                                                <span class="effect-1">PAGES</span>
-                                                <span class="effect-1">PAGES</span>
-                                            </span>
-                                        </a>
+                                            </li>
+                                        <?php endwhile; ?>
+                                    </ul>
+                                <?php endif; ?>
 
-                                    </li>
-                                    <li class="">
-                                        <a href="#">
-                                            <span class="link-effect">
-                                                <span class="effect-1">PORTFOLIO</span>
-                                                <span class="effect-1">PORTFOLIO</span>
-                                            </span>
-                                        </a>
-
-                                    </li>
-                                    <li class="">
-                                        <a href="#">
-                                            <span class="link-effect">
-                                                <span class="effect-1">BLOG</span>
-                                                <span class="effect-1">BLOG</span>
-                                            </span>
-                                        </a>
-
-                                    </li>
-                                    <li>
-                                        <a href="contact.html">
-                                            <span class="link-effect">
-                                                <span class="effect-1">CONTACT</span>
-                                                <span class="effect-1">CONTACT</span>
-                                            </span>
-                                        </a>
-                                    </li>
-                                </ul>
                             </nav>
                             <div class="navbar-right d-inline-flex d-lg-none">
                                 <button type="button" class="menu-toggle sidebar-btn">
@@ -165,16 +143,25 @@
                                 </button>
                             </div>
                         </div>
-                        <div class="col-auto d-none d-lg-block">
-                            <div class="header-button">
-                                <a href="project.html" class="btn">
-                                    <span class="link-effect">
-                                        <span class="effect-1">WORKS WITH US</span>
-                                        <span class="effect-1">WORKS WITH US</span>
-                                    </span>
-                                </a>
+
+                        <?php
+                        $header_cta = get_field('header_cta', 'option');
+                        if ($header_cta) :
+                            $link_url = $header_cta['url'];
+                            $link_title = $header_cta['title'];
+                            $link_target = $header_cta['target'] ? $header_cta['target'] : '_self';
+                        ?>
+                            <div class="col-auto d-none d-lg-block">
+                                <div class="header-button">
+                                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>" class="btn">
+                                        <span class="link-effect">
+                                            <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                            <span class="effect-1"><?php echo esc_html($link_title); ?></span>
+                                        </span>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
